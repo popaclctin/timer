1be37b5f7c9885d87c60ffd7f3f7994ba657dfcb
Claudiu Popa <popa.clctin@gmail.com>
2020-08-29 13:30:27 +0300

    Add Timer class constructor and timer layout

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+  }
+
+  start() {
+    console.log("The timer has started!");
+  }
+}
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
