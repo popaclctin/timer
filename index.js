182ba1ad9c399bbabfdf4a428730b0531df469b9
Claudiu Popa <popa.clctin@gmail.com>
2020-08-29 19:04:29 +0300

    Add svg circle

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
 
+const perimeter = 2 * Math.PI * circle.getAttribute("r");
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log("Timer started");
   },
   onTick() {
-    console.log("Timer just ticked down");
+    circle.setAttribute("stroke-dashoffset", currentOffset);
+    currentOffset = currentOffset - 1;
   },
   onComplete() {
     console.log("Timer just completed");
